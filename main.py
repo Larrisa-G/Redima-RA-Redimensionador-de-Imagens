6a3b3c47ca0173a3d9dbb22d19282cf2cdad6ca7
Larissa <larissagomes8020@gmail.com>
2024-05-06 17:57:34 -0300

    comandos para obter as dimenções da imagem e mostrar imagem na tela

---
+++ main.py
@@ -0,0 +1,8 @@
+from PIL import Image
+
+def escalar_imagem (imagem, escala):  
+    largura, altura = image. size
+
+imagem = Image.open(".\pepinof.png")
+imagem.show()
+escalar_imagem(imagem, 2.0)
