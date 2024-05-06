81d927a96df72880a0e34b5cc5fb5fba3c2841b2
Larissa <larissagomes8020@gmail.com>
2024-05-06 19:07:55 -0300

    adicionado o retorno da função e 'escalar_imagem'  e chamada da função 'escalar_quadrante'

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ def escalar_imagem (imagem, escala):
     nova_altura = int(altura * escala)
     nova_imagem = Image.new("RGB", (nova_largura, nova_altura)) # cria imagem vazia preenchida com pixels pretos por padrão
     
-    def escalar_quadrante(x, y, w, h)
+    def escalar_quadrante(x, y, w, h) :
         if w <= 1 or h <= 1 : #largura e altura do quadrante
             cor = imagem.getpixel((x,y)) #pega pixel da imagem original
-            nova_imagem.putpixel((int(x * escala), (y * escala))) # altera pixel da nova imagem
+            nova_imagem.putpixel((int(x * escala), int(y * escala)), cor) # altera pixel da nova imagem
         else:
             pt_medio_x = x +  w // 2 #calcula as coordenadas do ponto médio do quadrante
             pt_medio_y = y + h //2
@@ -19,7 +19,10 @@ def escalar_imagem (imagem, escala):
             escalar_quadrante(x, pt_medio_y, pt_medio_x - x, y + h - pt_medio_y)
             escalar_quadrante(pt_medio_x, pt_medio_y, x + w -pt_medio_x, y + h - pt_medio_y)
 
+    #linha inicial da função, ela que inicia o processo de escalonamento da imagem chamando a função 'escalar_quadrante' com as coordenadas e dimenções do quadrante inicial (que é a imagem toda) para que a função possa dividir a imagem em quadrante menores e aplicar a escala recursiva 
+    escalar_quadrante(0, 0, largura, altura)
+    return nova_imagem
 
 imagem = Image.open(".\pepinof.png")
-escalar_imagem(imagem, 2.0)
-imagem.show()
+nova_imagem = escalar_imagem(imagem, 2.0)
+nova_imagem.show()
