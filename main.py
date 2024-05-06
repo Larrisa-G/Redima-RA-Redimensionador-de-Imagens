88e56ea1bd2875c3124aff6719e9e867607664da
Larissa <larissagomes8020@gmail.com>
2024-05-06 18:23:55 -0300

    criação do caso base da função recursiva

--- main.py
+++ main.py
@@ -5,7 +5,14 @@ def escalar_imagem (imagem, escala):
     nova_largura = int(largura * escala)
     nova_altura = int(altura * escala)
     nova_imagem = Image.new("RGB", (nova_largura, nova_altura)) # cria imagem vazia preenchida com pixels pretos por padrão
-    return nova_imagem
+    
+    def escalar_quadrante(x, y, width, height)
+        if w <= 1 or h <= 1 :
+            cor = imagem.getpixel((x,y)) #pega pixel da imagem original
+            nova_imagem.putpixel((int(x * escala), (y * escala))) # altera pixel da nova imagem
+
+
+
 
 imagem = Image.open(".\pepinof.png")
 escalar_imagem(imagem, 2.0)
